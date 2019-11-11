01ffe86a957d929824e8017a095cf2f474974932
Nckflannery <Nckflannery@users.noreply.github.com>
2019-11-11 10:19:57 -0800

    adding html templates

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 '''Minimal flask app'''
-from flask import Flask
+from flask import Flask, render_template
 
 # Make the application
 app = Flask(__name__)
@@ -9,4 +9,10 @@ app = Flask(__name__)
 
 #now we define a function
 def hello():
-    return "Hello beautiful world!"
+    return render_template('home.html')
+
+# create another route
+@app.route('/about')
+
+def preds():
+    return render_template('about.html')
